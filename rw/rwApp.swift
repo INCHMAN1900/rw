cf0a72707a893e92ae77619093dc76977138f1f8
INCHMAN <asiafu1900@gmail.com>
2024-10-20 01:32:03 +0800

    Initial Commit

---
+++ rw/rwApp.swift
@@ -0,0 +1,17 @@
+//
+//  rwApp.swift
+//  rw
+//
+//  Created by Asia Fu on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct rwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rwTests/rwTests.swift
@@ -0,0 +1,17 @@
+//
+//  rwTests.swift
+//  rwTests
+//
+//  Created by Asia Fu on 2024/10/20.
+//
+
+import Testing
+@testable import rw
+
+struct rwTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
